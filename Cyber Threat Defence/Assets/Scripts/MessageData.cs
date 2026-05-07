25da92d1d17c32188be46064662af4a8437915e0
Jakob Thiberg <jakob.t.thiberg@gmail.com>
2026-05-07 13:59:34 +0200

    Messages manager

---
+++ Cyber Threat Defence/Assets/Scripts/MessageData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public enum MessageType { Normal, Phishing }
+public enum MessageAction { Reply, Report }
+
+[CreateAssetMenu(fileName = "MessageData", menuName = "Architect/Message Data")]
+public class MessageData : ScriptableObject
+{
+    public string sender;
+    public string subject;
+    [TextArea(3, 8)] public string body;
+    public MessageType type;
+}
